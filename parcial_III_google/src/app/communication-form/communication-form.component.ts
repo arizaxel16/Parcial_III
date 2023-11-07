63afda214368e55164d94bb0146de675e061bccd
arizaxel16 <110404509+arizaxel16@users.noreply.github.com>
2023-11-07 14:54:12 -0500

    component and style structure

---
+++ parcial_III_google/src/app/communication-form/communication-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-communication-form',
+  templateUrl: './communication-form.component.html',
+  styleUrls: ['./communication-form.component.css']
+})
+export class CommunicationFormComponent {
+
+}
---
+++ parcial_III_google/src/app/health-form/health-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-health-form',
+  templateUrl: './health-form.component.html',
+  styleUrls: ['./health-form.component.css']
+})
+export class HealthFormComponent {
+
+}
