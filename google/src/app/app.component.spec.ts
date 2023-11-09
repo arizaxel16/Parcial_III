7a5cd5b8133c46884b3c21402e45ad524e58767b
arizaxel16 <110404509+arizaxel16@users.noreply.github.com>
2023-11-08 22:30:59 -0500

    blank project

--- google/src/app/app.component.spec.ts
+++ google/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'parcial_III_google'`, () => {
+  it(`should have as title 'google'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('parcial_III_google');
+    expect(app.title).toEqual('google');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('parcial_III_google app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('google app is running!');
   });
 });
--- google/src/app/app.component.ts
+++ google/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'parcial_III_google';
+  title = 'google';
 }
